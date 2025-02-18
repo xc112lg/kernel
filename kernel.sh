11ef73102d47f105156a535eec85834e8061be4d
xc112lg <152837944+xc112lg@users.noreply.github.com>
2025-02-18 16:08:53 +0800

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,7 +1,7 @@
 
 git clone https://github.com/xc112lg/android_kernel_xiaomi_sm8150 -b patch-1 --depth 1
 cd android_kernel_xiaomi_sm8150
-git reset --hard
+git reset --hard origin/patch-1
 git pull
 chmod +x build.sh
 . build.sh"
